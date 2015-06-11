88c883c865ba1e83e8354a85295b1cff4cd33d5a
namzug82 <gabriel.strato@gmail.com>
2015-06-11 12:17:42 +0200

    modified YamlParser.php

--- src/Fw/Component/Routing/YamlParser.php
+++ src/Fw/Component/Routing/YamlParser.php
@@ -4,6 +4,13 @@ namespace Fw\Component\Routing\
 
 use Symfony\Component\Yaml\Parser;
 
-$yaml = new Parser();
+final class YamlParser implements Routing
+{   
+    private $yaml;
 
-$value = $yaml->parse(file_get_contents('/Yaml/routing.yml'));
+    public function __construct()
+    {
+        $this->yaml = new Parser();
+        return $this->yaml->parse(file_get_contents('../App/Component/Routing/Yaml/routing.yml'));
+    }
+}
