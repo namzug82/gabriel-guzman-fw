2f28aec96f00704a637efb98c838f72370edaa11
Gabriel <gabriel.strato@gmail.com>
2015-08-26 14:44:22 +0200

    added Cache component.

---
+++ src/Fw/Component/Cache/Cache.php
@@ -0,0 +1,23 @@
+<?php
+namespace Fw\Component\Cache;
+
+final class Cache
+{
+	private $cache;
+
+	public function __construct()
+	{
+		$this->cache = new \Memcached();
+		$this->cache->addServer('localhost', 11211);
+	}
+	
+	public function get($key)
+	{
+		return $this->cache->get($key);
+	}
+
+	public function set($key, $value, $expiration_in_seconds)
+	{
+		$this->cache->set($key, $value, 0, $expiration_in_seconds);
+	}
+}
